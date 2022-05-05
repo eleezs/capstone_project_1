daebb0a70b6abf01f8468f1b5d6659a81bc7fd70
eleezs <wilduche@gmail.com>
2022-05-05 15:13:29 +0100

    update on file

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const http = require('http');
 const os = require('os')
 const fs = require('fs')
 const url = require('url')
-const lookup = require('mime-types').lookup
 
 const port = 3000;
 
 const server = http.createServer((req, res) => {
 
   let parsedUrl = url.parse(req.url, true)
+
   // stripping the forward slashes at beginning and end
   let path = parsedUrl.path.replace(/^\/+|\/+$/g, "")
 
@@ -16,8 +16,8 @@ const server = http.createServer((req, res) => {
     path = "index.html"
   }
   console.log(`Requested path: ${path} `)
-  let file = __dirname + "/pages/" + path ;
-  console.log(file)
+  let file = __dirname + "/pages/" + path;
+  
   fs.readFile(file, (err, content) => {
     if(err) {
       console.log(`File Not Found ${file}`)
@@ -27,7 +27,6 @@ const server = http.createServer((req, res) => {
     }
     else if(path == "about.html"){
       res.writeHead(200, {"Content-Type": "text/html" })
-      res.end()
     }
 
     else if(path == "sys"){
@@ -57,36 +56,6 @@ const server = http.createServer((req, res) => {
     res.end(content)
   })
 })
-
-// server.get('/', (req, res) => {
-//   res.setHeader("Content-Type", "text/html")
-//   // res.writeHead(200)
-//   res.status(200).sendFile(path.join(__dirname+'/pages/index.html'))
-// });
-
-// server.get('/about', (req, res) => {
-//   res.setHeader("Content-Type", "text/html")
-//   res.status(200).sendFile(path.join(__dirname+'/pages/about.html'))
-// });
-
-// server.get('/sys', (req, res) => {
-//   let osInfo = {
-//     hostname: os.hostname(),
-//     platform: os.platform(),
-//     architecture: os.arch(),
-//     numberOfCPUS: os.cpus(),
-//     networkInterfaces: os.networkInterfaces(),
-//     uptime: os.uptime()
-//   }
-//   fs.writeFileSync(path.resolve(__dirname, 'osinfo.json'), JSON.stringify(osInfo))
-
-//   res.setHeader("Content-Type", "text/plain")
-//   res.status(201).end('Your OS info has been saved successfully')
-// })
-
-// server.use((req, res, next) => {
-  // res.status(404).sendFile(path.join(__dirname+'/pages/404.html'))
-// })
 
 
 server.listen(port, () => {
