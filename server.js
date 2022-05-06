c56550059949bf422933a4c9c67b40dfc6053e39
eleezs <wilduche@gmail.com>
2022-05-06 12:22:27 +0100

    modified

--- server.js
+++ server.js
@@ -12,43 +12,69 @@ const server = http.createServer((req, res) => {
   // stripping the forward slashes at beginning and end
   let path = parsedUrl.path.replace(/^\/+|\/+$/g, "")
 
+  let file;
+  
   if(path == ""){
     path = "index.html"
   }
+  if(path == "sys"){
+    let osInfo = {
+      hostname: os.hostname(),
+      platform: os.platform(),
+      architecture: os.arch(),
+      numberOfCPUS: os.cpus(),
+      networkInterfaces: os.networkInterfaces(),
+      uptime: os.uptime()
+    }
+    let osInfo1 = JSON.stringify(osInfo)
+    fs.writeFile('osinfo.json', osInfo1, (err) => {
+      if(err){
+       return console.log('An error Occured')
+      }
+      console.log('File saved')
+    })
+  
+    res.writeHead(201, {"Content-Type": "text/plain"})
+    res.end('Your OS info has been saved successfully')
+    // res.end()
+  }
+  else{
+    file = __dirname + "/pages/" + path;
+  }
   console.log(`Requested path: ${path} `)
-  let file = __dirname + "/pages/" + path;
+  
   
   fs.readFile(file, (err, content) => {
     if(err) {
-      console.log(`File Not Found ${file}`)
-      res.writeHead(404, {"Content-Type": "text/html" })
-      res.write('404.html')
-      // res.end("pages/404.html")
+     console.log(`File Not Found ${file}`)
+      res.writeHead(404 )
+      res.end()
     }
     else if(path == "about.html"){
       res.writeHead(200, {"Content-Type": "text/html" })
     }
-
-    else if(path == "sys"){
-      let osInfo = {
-        hostname: os.hostname(),
-        platform: os.platform(),
-        architecture: os.arch(),
-        numberOfCPUS: os.cpus(),
-        networkInterfaces: os.networkInterfaces(),
-        uptime: os.uptime()
-      }
-      let osInfo1 = JSON.stringify(osInfo)
-      fs.writeFile('osinfo.json', osInfo1, (err) => {
-        if(err){
-         return console.log('An error Occured')
-        }
-        console.log('File saved')
-      })
     
-      res.writeHead(201, {"Content-Type": "text/plain"})
-      res.end('Your OS info has been saved successfully')
-    }
+    // else if(path == "sys"){
+      // let osInfo = {
+      //   hostname: os.hostname(),
+      //   platform: os.platform(),
+      //   architecture: os.arch(),
+      //   numberOfCPUS: os.cpus(),
+      //   networkInterfaces: os.networkInterfaces(),
+      //   uptime: os.uptime()
+      // }
+      // let osInfo1 = JSON.stringify(osInfo)
+      // fs.writeFile('osinfo.json', osInfo1, (err) => {
+      //   if(err){
+      //    return console.log('An error Occured')
+      //   }
+      //   console.log('File saved')
+      // })
+    
+      // res.writeHead(201, {"Content-Type": "text/plain"})
+      // res.write('Your OS info has been saved successfully')
+      // res.end()
+    // }
 
     else if(path == "index.html"){
       res.writeHead(200, "Content-Type", "text/html")
